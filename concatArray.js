1db57ac2ef70bdb8720d0de286ffc3ec790a1dea
= <=>
2022-09-05 14:11:51 +0700

    test2

--- concatArray.js
+++ concatArray.js
@@ -1,7 +1,13 @@
 // const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  return array1.concat(array2)
+  for (elem of array2) {
+    array1.push(elem)
+  }
+  return array1
+}
+function concatArray(array1, array2, array3) {
+  return 0
 }
 
 console.log(concatArray([5, 10, 15], [2, 4, 6, 7]))
