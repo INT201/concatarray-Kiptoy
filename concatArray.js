eff590d59617404b289b6b8f2bb679ed4b186b3c
= <=>
2022-09-05 14:13:31 +0700

    test3

--- concatArray.js
+++ concatArray.js
@@ -6,9 +6,7 @@ function concatArray(array1, array2) {
   }
   return array1
 }
-function concatArray(array1, array2, array3) {
-  return 0
-}
+
 
 console.log(concatArray([5, 10, 15], [2, 4, 6, 7]))
 // module.exports = concatArray
